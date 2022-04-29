623d35b7eb8785af8397dd4d8aa218909748530c
hdj <20634hdj@naver.com>
2022-04-29 10:13:34 +0900

    day0429

--- day0428/src/Exercise7_19.java
+++ day0428/src/Exercise7_19.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /*
  * [ ] 실행결과
 	잔액이 부족하여 Computer을/를 살 수 없습니다.
@@ -35,25 +37,14 @@ class Exercise7_19 {
 	1.3 .(add ) 장바구니에 구입한 물건을 담는다 메서드 호출
 	Java 3 - 의 정석 판 연습문제 풀이 定石 57
 	*/
-		for (int i = 0; i < cart.length; i++) {
-			
-			if(p instanceof Tv) {
-				Tv tv = (Tv)p;
-				money = money - tv.price;
-				tv.add();
-			}
-			else if (p instanceof Computer) {
-				Computer cm = (Computer)p;
-				money = money - cm.price;
-				cm.add();
-			}
-			else if (p instanceof Audio) {
-				Audio ad = (Audio)p;
-				money = money - ad.price;				
-				ad.add();
-			}
+		if (money<p.price) {
+			System.out.println("잔액이 부족하여 " + p+"을/를 살 수 없습니다.");
+			return;
 		}
+		money -= p.price;
+		add(p);
 	}
+
 		
 	void add(Product p) {
 			
@@ -65,26 +56,36 @@ class Exercise7_19 {
 	1.1.3 . 새로운 장바구니와 기존의 장바구니를 바꾼다
 	1.2 (cart) . i 1 . 물건을 장바구니 에 저장한다 그리고 의 값을 증가시킨다
 	*/
-		
-		for (int i = 0; i < cart.length; i++) {
-			if (i>=cart.length) {
-				for (int j = 0; j < cart.length*2; j++) {
-					
-				}
-			}
-		}
 	
+	if (i>=cart.length) {
+		Product[] newCart = new Product[cart.length*2];
+		System.arraycopy(cart, 0, newCart, 0, cart.length);
+		cart = newCart;
+	}	
+	cart[i++] = p;
+		
 	} // add(Product p)
 		
 	void summary() {
 			
+		
 	/*
 	(3) . 아래의 로직에 맞게 코드를 작성하시오
 	1.1 . 장바구니에 담긴 물건들의 목록을 만들어 출력한다
 	1.2 . 장바구니에 담긴 물건들의 가격을 모두 더해서 출력한다
 	1.3 (money) . 물건을 사고 남은 금액 를 출력한다
 	 */
+				
+		int sum = 0;
+		
+		for (int i = 0; i < cart.length; i++) {
 			
+			sum += cart[i].price;
+
+		}
+		System.out.println("구입한 물건 : " + Arrays.toString(cart));
+		System.out.println(sum);
+		System.out.println(money);
 		} // summary()
 	}
 	
