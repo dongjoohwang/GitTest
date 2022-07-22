2aeb3b5fcdf97450648c4a122bbb64a2da094ce9
hdj <20634hdj@naver.com>
2022-07-22 21:51:44 +0900

    0722

--- 병원 홈페이지/src/test/java/org/hospital/controller/HospitalControllerTests.java
+++ 병원 홈페이지/src/test/java/org/hospital/controller/HospitalControllerTests.java
@@ -1,5 +1,143 @@
 package org.hospital.controller;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"file:src/main/webapp/WEB-INF/spring/root-context.xml",
+							"file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml"})
+@WebAppConfiguration
+@Log4j
 public class HospitalControllerTests {
 
+	@Autowired
+	private WebApplicationContext ctx;
+	
+	private MockMvc mvc;
+	
+	@Before
+	public void setup() {
+		this.mvc = MockMvcBuilders.webAppContextSetup(ctx).build();
+	}
+	
+	@Test
+	public void testList() throws Exception {
+		log.info(
+				mvc.perform(MockMvcRequestBuilders.get("/board/list"))
+				.andReturn()
+				.getModelAndView()
+				.getModelMap()
+				);
+	}
+	
+//	 hosname varchar2(200) not null,         -- 병원 이름    primary key
+//	    address varchar2(2000) not null,        -- 주소
+//	    phone varchar2(50) not null,            -- 전화번호
+//	    subject varchar2(50) not null,          -- 진료과목
+//	    workday varchar2(50) not null,          -- 진료일
+//	    opentime varchar2(100) not null,        --진료 시간(시작)
+//	    closetime varchar2(100) not null,       --진료 시간(종료)
+//	    holiday varchar(50) not null,           -- 휴무
+//	    holisubject varchar(50) not null,       -- 휴진 안내
+//	    location varchar(50) not null           -- 병원 위치
+	
+	@Test
+	public void testRegister() throws Exception {
+		String resultPage =
+				mvc.perform(MockMvcRequestBuilders.post("/board/register")
+						.param("hosname", "에센스 산부인과의원")
+						.param("address", "경기도 의정부시 오목로 205번길 1 4층 401호")
+						.param("phone", "031-852-7580")
+						.param("subject", "산부인과")
+						.param("workday", "월~토")
+						.param("opentime", "09:00")
+						.param("closetime", "18:00(토요일 : 13:00)")
+						.param("holiday", "일")
+						.param("holisubject", "전부 휴진")
+						.param("location", "에센스 산부인과의원"))
+						.andReturn()
+						.getModelAndView()
+						.getViewName();
+				log.info(resultPage);
+				
+	}
+	
+	@Test
+	public void testGet() throws Exception {
+		log.info(
+				mvc.perform(MockMvcRequestBuilders.get("/board/get")
+						.param("hospitalslist", "1"))
+						.andReturn()
+						.getModelAndView()
+						.getModelMap()
+				);
+	}
+	
+	@Test
+	public void testRemove() throws Exception {
+		String resultPage = 
+				mvc.perform(MockMvcRequestBuilders.post("/board/remove")
+				.param("hospitalslist", "2"))
+				.andReturn()
+				.getModelAndView()
+				.getViewName();
+		log.info(resultPage);
+	}
+	
+	@Test
+	public void testModify() throws Exception {
+		String resultPage = 
+				mvc.perform(MockMvcRequestBuilders.post("/board/modify")
+						.param("hosname", "상동아산 정형외과의원")
+						.param("address", "경기도 부천시 원미구 상동로 113 송재프라자 302호, 303호")
+						.param("phone", "032-327-5916")
+						.param("subject", "정형외과")
+						.param("workday", "월~토")
+						.param("opentime", "09:00")
+						.param("closetime", "20:00(토요일 : 15:00)")
+						.param("holiday", "일")
+						.param("holisubject", "전부 휴진")
+						.param("location", "상동아산 정형외과의원"))
+				.andReturn()
+				.getModelAndView()
+				.getViewName();
+		log.info(resultPage);
+	}
+	
+	@Test
+	public void testListPaging() throws Exception {
+		log.info(
+				mvc.perform(MockMvcRequestBuilders.get("/board/list")
+				.param("pageNum", "1")
+				.param("amount", "5"))
+				.andReturn()
+				.getModelAndView()
+				.getModelMap()
+				);
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
--- 병원 홈페이지/src/test/java/org/hospital/mapper/HospitalMapperTests.java
+++ 병원 홈페이지/src/test/java/org/hospital/mapper/HospitalMapperTests.java
@@ -1,5 +1,183 @@
 package org.hospital.mapper;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.hospital.criterial.HospitalCriterial;
+import org.hospital.dao.PageDAO;
+import org.hospital.vo.HospitalVO;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
 public class HospitalMapperTests {
 
+	@Autowired
+	private HospitalMapper mapper;
+	
+	@Test
+	public void testGetList() {
+		for(HospitalVO vo : mapper.getList()) {
+			log.info(vo);
+		}
+	}
+	
+//	 hosname varchar2(200) not null,         -- 병원 이름    primary key
+//    address varchar2(2000) not null,        -- 주소
+//    phone varchar2(50) not null,            -- 전화번호
+//    subject varchar2(50) not null,          -- 진료과목
+//    workday varchar2(50) not null,          -- 진료일
+//    opentime varchar2(100) not null,        --진료 시간(시작)
+//    closetime varchar2(100) not null,       --진료 시간(종료)
+//    holiday varchar(50) not null,           -- 휴무
+//    holisubject varchar(50) not null,       -- 휴진 안내
+//    location varchar(50) not null           -- 병원 위치
+	
+	@Test
+	public void testInsert() {
+		HospitalVO vo = new HospitalVO();
+		vo.setHosname("강의원");
+		vo.setAddress("경기도 고양시 일산동구 중앙로 1275번길 38-10 우림로데오스위트 209");
+		vo.setPhone("031-919-2568");
+		vo.setSubject("소아청소년학과");
+		vo.setWorkday("월~토");
+		vo.setOpentime("09:00(목 : 14:00)");
+		vo.setClosetime("18:30(목 : 21:00, 토 : 15:00)");
+		vo.setHoliday("일");
+		vo.setHolisubject("전부 휴진");
+		vo.setLocation("강의원");
+		mapper.insert(vo);
+		log.info(vo);
+	}
+	
+	@Test
+	public void testSelectKey() {
+		HospitalVO vo = new HospitalVO();
+		vo.setHosname("강의원");
+		vo.setAddress("경기도 고양시 일산동구 중앙로 1275번길 38-10 우림로데오스위트 209");
+		vo.setPhone("031-919-2568");
+		vo.setSubject("소아청소년학과");
+		vo.setWorkday("월~토");
+		vo.setOpentime("09:00(목 : 14:00)");
+		vo.setClosetime("18:30(목 : 21:00, 토 : 15:00)");
+		vo.setHoliday("일");
+		vo.setHolisubject("전부 휴진");
+		vo.setLocation("강의원");
+		mapper.insertSelectKey(vo);
+		log.info(vo);
+	}
+	
+	@Test
+	public void testread() {
+		HospitalVO vo = mapper.read(1L);
+		log.info(vo);
+	}
+	
+	@Test
+	public void testdelete() {
+		log.info("delete count : " + mapper.delete(1L));
+	}
+	
+	@Test
+	public void testupdate() {
+		HospitalVO vo = new HospitalVO();
+		vo.setHospitalslist(4L);
+		vo.setHosname("강의원");
+		vo.setAddress("경기도 고양시 일산동구 중앙로 1275번길 38-10 우림로데오스위트 209");
+		vo.setPhone("031-919-2568");
+		vo.setSubject("소아청소년학과");
+		vo.setWorkday("월~토");
+		vo.setOpentime("09:00(목 : 14:00)");
+		vo.setClosetime("18:30(목 : 21:00, 토 : 15:00)");
+		vo.setHoliday("일");
+		vo.setHolisubject("전부 휴진");
+		vo.setLocation("강의원");
+		log.info("update : " + mapper.update(vo));
+	}
+	
+	@Test
+	public void testPaging() {
+		HospitalCriterial hc = new HospitalCriterial();
+		hc.setPageNum(1);
+		hc.setAmount(5);
+		List<HospitalVO> list = mapper.getListWithPagging(hc);
+		log.info(list);
+	}
+	@Test
+	public void testPageDTO() {
+		HospitalCriterial hc = new HospitalCriterial();
+		hc.setPageNum(1);
+		hc.setAmount(5);
+		PageDAO dao = new PageDAO(hc, 1);
+		log.info(dao);
+	}
+	
+	@Test
+	public void testSearchTest() {
+		Map<String, String> map = new HashMap<>();
+		map.put("H", "연세권마취통증의학과의원");
+		map.put("A", "경기도 고양시 일산동구 중앙로 1275번길 38-10 우림로데오스위트 209");
+		map.put("S", "소아청소년학과");
+		Map<String, Map<String, String>> outer = new HashMap<>();
+		
+		outer.put("map", map);
+		List<HospitalVO> list = mapper.searchTest(outer);
+		log.info(list);
+	}
+	
+	@Test
+	public void testSearchPaging() {
+		HospitalCriterial hc = new HospitalCriterial();
+		hc.setType("HAS");
+		hc.setKeyword("연세");
+		List<HospitalVO> list = mapper.getListWithPagging(hc);
+		log.info(list);
+	}
+	
+	@Test
+	public void testTotal() {
+		HospitalCriterial hc = new HospitalCriterial();
+		hc.setType("T");
+		hc.setKeyword("새로");
+		
+		int count = mapper.getTotalCount(hc);
+		log.info("----------------");
+		log.info("count : " + count);
+	}
 }
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
