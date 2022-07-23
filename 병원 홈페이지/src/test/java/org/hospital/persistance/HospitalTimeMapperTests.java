00dfe367ae9b11f960dadbee100b97b223faf1f1
hdj <20634hdj@naver.com>
2022-07-23 15:08:25 +0900

    0723

--- 병원 홈페이지/src/test/java/org/hospital/persistance/HospitalTimeMapperTests.java
+++ 병원 홈페이지/src/test/java/org/hospital/persistance/HospitalTimeMapperTests.java
@@ -1,5 +1,47 @@
 package org.hospital.persistance;
 
+import org.hospital.mapper.HospitalTimeMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
 public class HospitalTimeMapperTests {
 
+	@Autowired
+	private HospitalTimeMapper timemapper;
+	
+	@Test
+	public void testGetTime() {
+		log.info("--------------------");
+		log.info("info1 : " + timemapper.getClass().getName());
+		log.info("info2 : " + timemapper.getTime());
+	}
+	
+	@Test
+	public void testGetTime2() {
+		log.info("--------------------");
+		log.info("info1 : " + timemapper.getClass().getName());
+		log.info("getTime2 : " + timemapper.getTime2());
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- 병원 홈페이지/src/test/java/org/hospital/persistance/JDBCTests.java
+++ 병원 홈페이지/src/test/java/org/hospital/persistance/JDBCTests.java
@@ -1,5 +1,43 @@
 package org.hospital.persistance;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
 public class JDBCTests {
 
+	static {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testConnection() {
+		try(Connection conn = DriverManager.getConnection(
+				"jdbc:oracle:thin:@localhost:1521:xe",
+				"system",
+				"1234")) {
+			log.info("-------------");
+			log.info(conn);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
+
+
+
+
+
+
+
+
+
+
